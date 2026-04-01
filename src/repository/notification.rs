3d53b0f09ac8bac4e27514ed4677c2315468cd08
jebs-tech <bungasaragih688@gmail.com>
2026-04-01 17:46:06 +0700

    Fix RwLock import in NotificationRepository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -1,5 +1,5 @@
+use std::sync::RwLock;
 use lazy_static::lazy_static;
-use rocket::tokio::sync::RwLock;
 use crate::model::notification::Notification;
 
 lazy_static! {
@@ -10,17 +10,13 @@ pub struct NotificationRepository;
 
 impl NotificationRepository {
     pub fn add(notification: Notification) -> Notification {
-        let mut data = rocket::tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(NOTIFICATIONS.write());
-        data.push(notification.clone());
+        NOTIFICATIONS.write().unwrap()
+            .push(notification.clone());
         return notification;
     }
 
     pub fn list_all_as_string() -> Vec<String> {
-        let data = rocket::tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(NOTIFICATIONS.read());
-        return data.iter().map(|f: &Notification| format!("{}", f.clone())).collect();
+        return NOTIFICATIONS.read().unwrap()
+            .iter().map(|f: &Notification| format!("{}", f.clone())).collect();
     }
 }
